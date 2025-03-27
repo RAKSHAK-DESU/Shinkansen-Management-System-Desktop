f837fb27f603f8e7048f53df9c362d00fff0f10d
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-03-27 23:33:28 +0530

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static JFrame frame;
+    private static JPanel table;
+    private static GridLayout gridLayout;
     public static void main(String[] args) {
+        frame = new JFrame("新幹線管理システム");
+        frame.setLocationRelativeTo(null);
+        frame.setSize(1050,650);
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().setBackground(Color.decode("#B3FFFF"));
+        JPanel panel = new JPanel(new BorderLayout(20,20));
+        panel.setBackground(null);
+        panel.setBorder(BorderFactory.createEmptyBorder(50,50,30,50));
+
+        JLabel title = new JLabel("新幹線管理システムへようこそ");
+        title.setForeground(Color.decode("#012010"));
+        title.setFont(new Font(null,Font.BOLD,35));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        panel.add(title,BorderLayout.NORTH);
+
+        gridLayout= new GridLayout(6,1);
+        table = new JPanel(gridLayout);
+        table.setBackground(Color.decode("#EBFFD8"));
+
+        frame.setVisible(true);
+    }
+
+    private static JPanel row(int index,Trip trip){
+        JPanel row = new JPanel(new FlowLayout(FlowLayout.CENTER,20,20));
+        if (index%2==0) row.setBackground(Color.decode("#e5e5e5"));
+        else row.setBackground(Color.decode("#EEEEEE"));
+        JLabel train= JLabel(trip.getTrain().getDescription(),100);
+        row.add(train);
+
+        JLabel start = JLabel(trip.getStartStation(),100);
+        row.add(start);
+
+        JLabel dest= JLabel(trip.getDestination(),100);
+        row.add(dest);
+
+        JLabel date = JLabel(trip.getDate(),150);
+        row.add(date);
+
+        JLabel deptTime = JLabel(trip.getDepartureTime(), 65);
+        row.add(deptTime);
+
+        JLabel arrTime= JLabel(trip.getArrivalTime(),65);
+        row.add(arrTime);
+
+        JLabel price = JLabel(trip.getPrice()+"$",50);
+        row.add(price);
+
+        String st="Booked";
+        if(trip.getTrain().getCapacity()>trip.getbookedSeats()) st="Available";
+
+        JLabel status = JLabel(st,100);
+        return row;
+    }
+    private static JLabel JLabel(String text,int width){
+        JLabel label= new JLabel(text);
+        label.setPreferredSize(new Dimension(width,20));
+        label.setFont(new Font(null,Font.PLAIN,20));
+        label.setForeground(Color.decode("#13678A"));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        return label;
 
     }
 }
--- src/Trip.java
+++ src/Trip.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class Trip {
     private int id;
@@ -10,4 +13,83 @@ public class Trip {
     private LocalDate date;
     private int bookedSeats;
     private double price;
+    private Employee driver;
+    private Train train;
+    private ArrayList<Passenger> passengers;
+
+    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private DateTimeFormatter timeFormatter= DateTimeFormatter.ofPattern("HH:mm");
+
+    public Trip(){}
+
+    public int getID(){
+        return id;
+    }
+    public void setID(int id){
+        this.id=id;
+    }
+    public String getStartStation(){
+        return start;
+    }
+    public void setStartStation(String Start){
+        this.start=start;
+    }
+    public String getDestination(){
+        return destination;
+    }
+    public void setDestination(String destination){
+        this.destination=destination;
+    }
+    public String getDepartureTime(){
+        return departureTime.format(timeFormatter);
+    }
+    public void setDepartureTime(String departureTime){
+        this.departureTime = LocalTime.parse(departureTime,timeFormatter);
+    }
+    public String getArrivalTime(){
+        return arrivalTime.format(timeFormatter);
+    }
+    public void setArrivalTime(String arrivalTime){
+        this.arrivalTime= LocalTime.parse(arrivalTime,timeFormatter);
+    }
+    public String getDate(){
+        return date.format(dateFormatter);
+    }
+    public void setDate(String date){
+        this.date= LocalDate.parse(date,dateFormatter);
+    }
+    public int getbookedSeats(){
+        return bookedSeats;
+    }
+    public void setbookedSeats(int bookedSeats){
+        this.bookedSeats= bookedSeats;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price= price;
+    }
+    public Employee getDriver(){
+        return driver;
+    }
+    public void setDriver(Employee driver){
+        this.driver = driver;
+    }
+    public Train getTrain(){
+        return train;
+    }
+    public void setTrain(Train train){
+        this.train= train;
+    }
+    public ArrayList<Passenger> getPassengers(){
+        return passengers;
+    }
+    public void setPassengers(ArrayList<Passenger> passengers){
+        this.passengers= passengers;
+    }
+    public void addPassengers(Passenger passenger){
+        passengers.add(passenger);
+    }
+
 }
