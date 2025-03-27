2b0eb2e07bcaf7ebc3603e113d27a44961a316ec
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-03-27 18:33:39 +0530

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+    private int id;
+    private String name;
+    private String email;
+    private String tel;
+    private double salary;
+    private String position;
+
+    public Employee(){}
+}
--- src/Passenger.java
+++ src/Passenger.java
@@ -5,4 +5,36 @@ public class Passenger {
     private String email;
 
     public Passenger() {}
+
+    public int getID(){
+        return id;
+    }
+
+    public void setID(int id){
+        this.id=id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name= name;
+    }
+
+    public String getTel(){
+        return tel;
+    }
+
+    public void setTel(String tel){
+        this.tel=tel;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
